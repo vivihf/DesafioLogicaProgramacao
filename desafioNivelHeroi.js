a4e53f3edd1f2e859462001a9a2f91c94c14967c
vivihf <viviads1203@gmail.com>
2024-04-08 18:59:47 -0300

    Código desafio nível herói

---
+++ desafioNivelHeroi.js
@@ -0,0 +1,23 @@
+let nomeHeroi = "Exemplo";
+let xpHeroi = 7500; 
+let nivelHeroi;
+
+if (xpHeroi <= 1000) {
+    nivelHeroi = "Ferro";
+} else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+    nivelHeroi = "Bronze";
+} else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+    nivelHeroi = "Prata";
+} else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+    nivelHeroi = "Ouro";
+} else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+    nivelHeroi = "Platina";
+} else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+    nivelHeroi = "Ascendente";
+} else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+    nivelHeroi = "Imortal";
+} else {
+    nivelHeroi = "Radiante";
+}
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelHeroi);
